5610b2386f75bedd952d3ca8b115f9de7345349c
CYixiang <150227665+CYixiang@users.noreply.github.com>
2023-11-25 18:15:32 +0800

    Create setting.js

---
+++ setting.js
@@ -0,0 +1,4 @@
+var FHIRapiURL="https://fhir.tcumi.com:58443/r5/fhir"; //portal = person, org, patient(帳戶管理)
+var FHIRrootURL ="https://hapi.fhir.org/baseR4"; //repo = patient, organization, observation, condition(定義:個人健康紀錄PHR)
+var fhirServerUrl = 'https://fhir.tcumi.com:58443/r5/fhir/Patient';
+var apiURL = 'https://fhir.tcumi.com:58443/r5/fhir/Condition'; // FHIR server endpoint
